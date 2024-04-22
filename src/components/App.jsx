2a86a40cd04e432879477687cf609f6b61fa9c42
jsmolos <molosj@yahoo.com>
2024-04-22 21:49:29 +0800

    Profile&stat update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
-import friends from '../data/friends.json';
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import profileData from '../data/profileData.json';
+import statisticsData from '../data/statisticsData.json';
+import friends from '../data/friends.json';
 import items from '../data/transactions.json';
+
+
 
 
 export const App = () => {
   return (
     <div>
-      <FriendList friends={friends} />
-      
-      <TransactionHistory items={items} />
+      {profileData && (
+        <Profile
+        username={profileData.username}
+        tag={profileData.tag}
+        location={profileData.location}
+        avatar={profileData.avatar}
+        stats={profileData.stats}
+        />
+      )}
 
+      {statisticsData && (
+        <Statistics title="Statistics" stats={statisticsData} />
+      )}
+      <FriendList friends={friends} />
+      <TransactionHistory items={items} />    
+      
     </div>
   );
 };
